9ea99594e6190687e9cdaa75aad1b17ee72b54b9
rakshitmakadia <31830409+rakshitmakadia@users.noreply.github.com>
2025-06-27 22:14:20 +0530

    Update select_from_custom.py

--- select_from_custom.py
+++ select_from_custom.py
@@ -6,7 +6,7 @@ def main():
     """
     Connects to the MySQL database using the connection object returned by mysqldb.get_mysql_conn(), and executes a select statement defined in a file.
 
-    The function takes in a file path to a SQL select statement as a command line argument.
+    The function takes in a file name of a SQL select statement as a command line argument. The file should be in the "sql" folder present in current directory
 
     If the select statement fails to execute, an error message will be printed with details of the error.
 
@@ -19,8 +19,9 @@ def main():
 
     filebase.create_local_tmp()
     sql_file = sys.argv[1]
+    sql_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sql", sql_file)
     conn = mysqldb.get_mysql_conn()
-    result = mysqldb.select_from_table(conn, sql_file, write_to_file=True)
+    result = mysqldb.select_from_table(conn, sql_path, write_to_file=True)
     # print(result)
     print(
         "Result written to file "
